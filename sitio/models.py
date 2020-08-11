64768af2e312b9c6f2da6a13864d07581fe49f26
Juan Pedro Fisanotti <fisadev@gmail.com>
2020-08-11 16:39:11 -0300

    Initial version, after what we saw in class

---
+++ sitio/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Noticia(models.Model):
+    titulo = models.CharField(max_length=50)
+    texto = models.CharField(max_length=200)
+    fecha = models.DateTimeField()
+    archivada = models.BooleanField(default=False)
+    denuncias = models.IntegerField(default=0)
+
+
